162248d638a324fbd43e0e98146239014f9b6d18
Harsh Kumar Bhartiya <harshkbhartiya@gmail.com>
2018-10-12 13:26:08 +0000

    Correct cdn, links and stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
+app.use(express.static(__dirname))
 let sentimentController = require('./controller/sentimentController.js')
 // let departmentController = require('./controller/departmentController.js')
 let departmentModel = require('./models/departmentModel.js')
